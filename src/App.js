a76368079ee705abd3ca3721141962789d95a2fb
Harinton <arizavargasharintondavid4@gmail.com>
2021-05-21 12:35:34 -0500

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            pokemons : [],
+            page: 50,
+        };
+    }
+
+
+    componentDidMount(){
+
+
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+        .then(response => response.json())
+        .then(name => this.setState({pokemons:name.results}));
+    }
+
+    
+    handleClick = () => {
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${this.state.page}`)
+            .then((response) => response.json())
+            .then((name) => {
+                const showMore = [...this.state.pokemons, ...name.results ]
+                this.setState({ ...this.state, page: this.state.page+50, pokemons: showMore });
+            });
+    }
+
+
+    render(){
+        const { pokemons } = this.state;
+
+        return(
+            <div className="App">
+                <CardList pokemons={pokemons}></CardList>
+                <button onClick={this.handleClick}>50+</button>
+            </div>
+            
+        );
+    }
 }
 
 export default App;
